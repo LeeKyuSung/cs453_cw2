645b51d57eddde160d3004144127f1d99b0f4c2b
LeeKyuSung <LeeKyuSung@DESKTOP-BIKHL2D>
2020-05-29 18:33:53 +0900

    ast 테스트, 개발 시작

--- src/covgen.py
+++ src/covgen.py
@@ -41,3 +41,13 @@ for x in body1:
         returns = x.returns
         type_comment = x.type_comment
 
+'''
+
+1. 파일 읽기
+2. 조건 맞는지 확인
+3. function def (test_me) 있는지 확인
+4. 해당 function 재귀로 tree 생성
+5. 각 노드 별로 테스트 케이스 생성
+-> 사용안하는 값은 뭘로 넣지? 일단 랜덤으로 하고 생각해보자
+
+'''
---
+++ src/node.py
@@ -0,0 +1,5 @@
+class node:
+    def __Init__(self):
+        self.type;
+        self.true_child;
+        self.false_child;
